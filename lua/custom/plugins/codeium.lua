3f4deec64427e7b1bb1a654ee08db9968751b724
Daniel Simionato <weseven@gmail.com>
2024-11-15 12:41:28 +0100

    Move to codeium.nvim

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,21 +1,51 @@
 return {
-  {
-    'Exafunction/codeium.vim',
-    event = 'VeryLazy',
-    config = function()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<C-g>', function()
-        return vim.fn['codeium#Accept']()
-      end, { expr = true })
-      vim.keymap.set('i', '<C-n>', function()
-        return vim.fn['codeium#CycleCompletions'](1)
-      end, { expr = true })
-      vim.keymap.set('i', '<C-p>', function()
-        return vim.fn['codeium#CycleCompletions'](-1)
-      end, { expr = true })
-      vim.keymap.set('i', '<C-x>', function()
-        return vim.fn['codeium#Clear']()
-      end, { expr = true })
-    end,
+  'Exafunction/codeium.nvim',
+  event = 'InsertEnter',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
   },
+  config = function()
+    require('codeium').setup {
+
+      virtual_text = {
+        enabled = true,
+
+        -- These are the defaults
+
+        -- Set to true if you never want completions to be shown automatically.
+        manual = false,
+        -- A mapping of filetype to true or false, to enable virtual text.
+        filetypes = {},
+        -- Whether to enable virtual text of not for filetypes not specifically listed above.
+        default_filetype_enabled = true,
+        -- How long to wait (in ms) before requesting completions after typing stops.
+        idle_delay = 75,
+        -- Priority of the virtual text. This usually ensures that the completions appear on top of
+        -- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
+        -- desired.
+        virtual_text_priority = 65535,
+        -- Set to false to disable all key bindings for managing completions.
+        map_keys = true,
+        -- The key to press when hitting the accept keybinding but no completion is showing.
+        -- Defaults to \t normally or <c-n> when a popup is showing.
+        accept_fallback = nil,
+        -- Key bindings for managing completions in virtual text mode.
+        key_bindings = {
+          -- Accept the current completion.
+          accept = '<C-g>',
+          -- Accept the next word.
+          accept_word = false,
+          -- Accept the next line.
+          accept_line = false,
+          -- Clear the virtual text.
+          clear = '<C-x>',
+          -- Cycle to the next completion.
+          next = '<C-n>',
+          -- Cycle to the previous completion.
+          prev = '<C-p>',
+        },
+      },
+    }
+  end,
 }
--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -4,7 +4,6 @@ return {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
-    event = 'VeryLazy',
     ft = 'lua',
     opts = {
       library = {
